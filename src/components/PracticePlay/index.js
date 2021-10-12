aed0166cc81b9dbf513640016cb224b0f5f951d3
wowill <qingsuen@vip.qq.com>
2021-10-13 01:19:09 +0800

    fix:

--- src/components/PracticePlay/index.js
+++ src/components/PracticePlay/index.js
@@ -4,35 +4,55 @@ import BoxNext from "components/BoxNext/index";
 import images from "assets/images/index";
 import "./styles.scss";
 
-const PracticePlay = (props) => {
+const PracticePlay = props => {
   const [classImage, setClassImage] = useState("");
 
   const { currentPage, onPushAction, page6 } = props;
-  const {
-    imageKids,
-    className,
-  } = props;
+  const { imageKids, className } = props;
 
   var content = page6.map((item, index) => {
+    window.addEventListener(`practicePlay_${item.image}`, () => {
+      setClassImage("active");
+    });
     if (item.active === false)
       return (
         <img
           className="item"
           alt={item.image}
           src={item.image}
-          onClick={(e) => {
+          onClick={e => {
             onPushAction(e, "play_audio", "audioFalse");
           }}
         />
       );
     return (
       <img
-        className={` item ${classImage ? classImage : ""} `}
+        className={` item ${
+          classImage || window[`classImage_${item.image}`]
+            ? classImage || window[`classImage_${item.image}`]
+            : ""
+        } `}
         alt={item.image}
         src={item.image}
-        onClick={(e) => {
-          onPushAction(e, "play_audio", "audioTrue");
-          setClassImage("active");
+        onClick={e => {
+          onPushAction(
+            e,
+            "play_audio",
+            "audioTrue",
+            `practicePlay_${item.image}`
+          );
+
+          // , () => {
+          //   window[`classImage_${item.image}`] = "avtive";
+          //   console.log(window[`classImage_${item.image}`]);
+          //   // setClassImage("active");
+          //   try {
+          //     setClassImage("active");
+          //   } catch (error) {
+          //     console.log(error);
+          //   }
+          // }
+
           setTimeout(() => {
             onPushAction(e, "play_audio", item.audio);
           }, 2000);
@@ -63,12 +83,10 @@ const PracticePlay = (props) => {
                 alt={images.icons.answer6}
                 src={images.icons.answer6}
               />
-              
             </div>
             <div className="star">{content}</div>
           </div>
         </div>
-       
       </div>
     );
   };
--- src/constants/App.js
+++ src/constants/App.js
@@ -1,9 +1,10 @@
 const pageCount = 34;
 
 const getPageData = () => {
-	const data = Array(pageCount).fill([]);
-	data[1] = [].concat(...Array(4).fill(""));
-	return data;
+  const data = Array(pageCount).fill([""]);
+  // This operation will cause the page to click 4 times to turn the page successfully.
+  // data[1] = [].concat(...Array(4).fill(""));
+  return data;
 };
 
 export const PAGE_DATA = getPageData();
